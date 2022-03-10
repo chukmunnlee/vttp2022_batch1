44c1eeb365fbe9fbcd4632230eb1de9449f919ed
chukmunnlee <chukmunnlee@gmail.com>
2022-03-10 11:45:36 +0800

    revision day 2 AM

--- revision1/src/main/java/vttp2022/ssf/revision1/controllers/RegisterController.java
+++ revision1/src/main/java/vttp2022/ssf/revision1/controllers/RegisterController.java
@@ -1,19 +1,25 @@
 package vttp2022.ssf.revision1.controllers;
 
 import java.util.Collection;
+import java.util.logging.Logger;
 
 import javax.annotation.PostConstruct;
 
+// FQCN
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import vttp2022.ssf.revision1.Revision1Application;
 import vttp2022.ssf.revision1.models.Registrations;
 import vttp2022.ssf.revision1.services.RegistrationService;
 
@@ -21,12 +27,46 @@ import vttp2022.ssf.revision1.services.RegistrationService;
 @RequestMapping(path="/register")
 public class RegisterController {
 
+    Logger logger = Logger.getLogger(Revision1Application.class.getName());
+    
     @Autowired
     private RegistrationService regSvc;
 
+    @Autowired
+    private ApplicationArguments args;
+
+    // @Value reads from application.properties
+    @Value("${app.version}")
+    String appVersion;
+
     @PostConstruct
     public void init() {
-        System.out.println("+++++ Creating controller");
+        logger.entering("init", "");
+        logger.info(">>>>> value of --data: %s\n".formatted(args.getOptionValues("data")));
+        logger.exiting("init", "");
+    }
+
+    @GetMapping("/version")
+    public String getApplicationVersion(Model model) {
+        // get version
+        String version = "v1beta1";
+        model.addAttribute("appVersion", appVersion);
+        return "version";
+    }
+
+    @GetMapping("/name/{username}")
+    public String getRegistrationByName(@PathVariable String username, Model model) {
+
+        Registrations r = regSvc.getUserByName(username);
+        if (null != r) {
+            // 200 OK
+            model.addAttribute("newReg", r);
+            return "registered";
+        } 
+
+        // 404 Not Found
+        model.addAttribute("username", username);
+        return "not_found";
     }
 
     @PostMapping(path={"", "/"}, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
---
+++ revision2/src/main/java/vttp2022/ssf/revision2/Revision2Application.java
@@ -0,0 +1,13 @@
+package vttp2022.ssf.revision2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Revision2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Revision2Application.class, args);
+	}
+
+}
