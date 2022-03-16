19caa596e1eca7d4c88e0562834c88fffb81c344
chukmunnlee <chukmunnlee@gmail.com>
2022-03-16 16:15:01 +0800

    day8 weather workshop

---
+++ day8-weather/src/main/java/vttp2022/ssf/day8weather/Day8WeatherApplication.java
@@ -0,0 +1,13 @@
+package vttp2022.ssf.day8weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day8WeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day8WeatherApplication.class, args);
+	}
+
+}
---
+++ day8-weather/src/test/java/vttp2022/ssf/day8weather/Day8WeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package vttp2022.ssf.day8weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day8WeatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
